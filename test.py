bdc179e93651199bd04d202b6eddcb8e014c7cff
Carlos <cgzlazaro@gmail.com>
2024-11-19 17:36:54 +0100

    v1.8.2 Añadidos comentarios

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     forecast_dict = index_instance.get_forecast_dict()
 
     forecast_instance = Forecast(data_dict, forecast_dict, start_calculation_date, end_calculation_date, horizons)
-    forecast_instance.run_single_forecast(method='lstm')
+    forecast_instance.run_single_forecast(method='random_forest')
 
     backtest_manager = BacktestManager(data_dict, forecast_dict, confidence_level)
     backtest_manager.run_backtest_multiquantile()
